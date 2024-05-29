5a7f8e39e6f8609a87e9a376a688955fca09470a
GabrielaEvtimova <147919001+GabrielaEvtimova@users.noreply.github.com>
2024-05-29 17:28:33 +0300

    Add webpack config file

--- src/server/server.ts
+++ src/server/server.ts
@@ -1,23 +1,22 @@
 import express from "express";
-import os from "node:os"
-import config from "./config"
+import os from "node:os";
+import config from "./config";
 
 const server = express();
 
-
 server.use(express.static("dist"));
 
-server.set("view engine", "ejs")
+server.set("view engine", "ejs");
 
 server.use("/", (req, res) => {
   res.render("index", {
-    content: "EJS is <b>cool</b>!"
-  })
+    initialContent: "Loading...",
+  });
 });
 
 server.listen(config.PORT, config.HOST, () => {
-
-  console.info(`Express server is listening at ${config.SERVER_URL}`, 
-  `Free memory is ${os.freemem() / 1024 / 1024}`);
-
+  console.info(
+    `Express server is listening at ${config.SERVER_URL}`,
+    `Free memory is ${os.freemem() / 1024 / 1024}`,
+  );
 });
