9ec824b39586215e4ac7ed05cdcbc2de9a06812a
GabrielaEvtimova <147919001+GabrielaEvtimova@users.noreply.github.com>
2024-05-30 03:45:47 +0300

    Refactoring - add components (Header & App)

---
+++ src/components/App/App.tsx
@@ -0,0 +1,12 @@
+import Header from "../Header/Header";
+import { useState, useEffect } from "react";
+
+const App = () => {
+  return (
+    <div className="container">
+      <Header message="Naming Contests" />
+    </div>
+  );
+};
+
+export default App;
