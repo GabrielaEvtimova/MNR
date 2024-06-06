afbe158d467cd77b91ee78c718846d2187bd0c5a
GabrielaEvtimova <147919001+GabrielaEvtimova@users.noreply.github.com>
2024-06-06 13:45:17 +0300

    Add fetchData function

--- src/components/ContestsList/ContestsList.tsx
+++ src/components/ContestsList/ContestsList.tsx
@@ -1,17 +1,13 @@
 import React from "react";
 import Contest from "../Contest/Contest";
-import axios from "axios";
 import { useEffect, useState } from "react";
-import { API_SERVER_URL } from "../../public-config";
+import { fetchContests } from "../../api-client";
 
 const ContestsList = ({ initialContests }) => {
-  
   const [contests, setContests] = useState(initialContests);
 
   useEffect(() => {
-    axios.get(`${API_SERVER_URL}/contests`).then((res) => {
-      setContests(res.data.contests);
-    });
+    fetchContests().then((data) => setContests(data.contests));
   }, []);
 
   return (
