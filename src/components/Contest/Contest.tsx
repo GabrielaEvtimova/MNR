dfdb9a2c2153f749c8c9b1656e7adfa2af32c37b
GabrielaEvtimova <147919001+GabrielaEvtimova@users.noreply.github.com>
2024-06-13 11:04:02 +0300

    Add handling data for proposed names in each contest

--- src/components/Contest/Contest.tsx
+++ src/components/Contest/Contest.tsx
@@ -25,6 +25,21 @@ const Contest = ({ initialContest, onContestListClick }) => {
         <div className="title">Contest Description</div>
         <div className="description">{contest.description}</div>
 
+        <div className="title">Proposed Names</div>
+        <div className="body">
+          {contest.names?.length > 0 ? (
+            <div className="list">
+              {contest.names.map((proposedName) => (
+                <div className="item" key={proposedName.id}>
+                  {proposedName.name}
+                </div>
+              ))}
+            </div>
+          ) : (
+            <div>No names proposed yet</div>
+          )}
+        </div>
+
         <a
           href="/"
           className="link"
